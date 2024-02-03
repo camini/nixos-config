30f8e7bd3a4d597fd685683e0a6c6492f7d00ff3
Cammi <camini57@gmail.com>
2024-02-04 00:34:03 +0100

    clarification de la config

--- configuration.nix
+++ configuration.nix
@@ -1,49 +1,42 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    [ ./hardware-configuration.nix
       ./VM.nix
       ];
 
-  # Setup bash completion
+  # Completion bash.
   programs.bash.enableCompletion = true;
 
-  # Bootloader.
+  # Boot.
   boot.plymouth.enable = true;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  
-  
-  services.xserver.videoDrivers = [ "amdgpu" ];
-  hardware.opengl.driSupport = true; # This is already enabled by default
-  hardware.opengl.driSupport32Bit = true; # For 32 bit applications
-     
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
   services.fstrim.enable =  true;
-  fileSystems."/home/cammi/Jeux" =
+
+  # GPU.
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  hardware.opengl.driSupport = true; 
+  hardware.opengl.driSupport32Bit = true; 
+     
+  # Nom de la machine.
+  networking.hostName = "nixos"; 
+  
+  # Montage disque Jeux.
+    fileSystems."/home/cammi/Jeux" =
   { device = "/dev/disk/by-label/Jeux";
     fsType = "ext4";
   };
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  
+  # Activation réseau.
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  # Timezone.
   time.timeZone = "Europe/Paris";
 
-  # Select internationalisation properties.
+  # Locale.
   i18n.defaultLocale = "fr_FR.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -58,26 +51,23 @@
     LC_TIME = "fr_FR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
+  # X11.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
+  # Gnome.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  # Configure keymap in X11
+  # Keymap sur X11.
   services.xserver = {
     xkb.layout = "fr";
     xkb.variant = "azerty";
   };
 
-  # Configure console keymap
+  # Keymap console.
   console.keyMap = "fr";
 
-  # Enable CUPS to print documents.
-  #services.printing.enable = true;
-
-  # Enable sound with pipewire.
+  # Pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -86,64 +76,50 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Compte utilisateur.
   users.users.cammi = {
     isNormalUser = true;
     description = "cammi";
     extraGroups = [ "video" "audio" "usb" "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
   };
 
-  # Enable automatic login for the user.
-  # services.xserver.displayManager.autoLogin.enable = true;
-  # services.xserver.displayManager.autoLogin.user = "cammi";
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  # systemd.services."getty@tty1".enable = false;
-  # systemd.services."autovt@tty1".enable = false;
-
+  # Nettoyage Nix.
   nix.optimise.automatic = true;
+  nix.settings.auto-optimise-store = true;
   nix.gc = {
      automatic = true;
      dates = "weekly";
      options = "--delete-older-than 7d";
    };
 
-  
-  # Allow unfree packages
+  # Paquets non libres.
   nixpkgs.config.allowUnfree = true;
-  nix.settings.auto-optimise-store = true;
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  
+  # Mes applications.
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+   # Editeur/code  
 	vscode
 	bash-completion
 	git
+   # Theme/visuel
 	plymouth
-	mangohud
-	adw-gtk3
-	qogir-icon-theme
-	firefox
+  adw-gtk3
+  qogir-icon-theme
+   # Internet/mail
+  firefox
 	thunderbird
-	neofetch
+	discord
+   # Shell
+  neofetch
 	btop
 	discord
+   # Emulateur
 	yuzu-mainline
+   # Video
 	vlc
+   # Gnome extensions
 	gnome.gnome-tweaks
 	gnomeExtensions.blur-my-shell
 	gnomeExtensions.spotify-tray
@@ -151,13 +127,15 @@
 	gnomeExtensions.dash-to-dock	
   ];
   
+  # Installation de Steam.
   programs.steam = {
   enable = true;
-  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  remotePlay.openFirewall = true; 
+  dedicatedServer.openFirewall = true; 
 };
+ 
+  # Nettoyage de Gnome.
   services.xserver.excludePackages = [ pkgs.xterm ];
-
   environment.gnome.excludePackages = with pkgs; [
     gnome.gnome-music gnome-tour gnome-photos gnome.gnome-weather 
     gnome.totem gnome.tali gnome.iagno gnome.hitori gnome.atomix gnome.simple-scan
@@ -166,35 +144,11 @@
     gnome.gnome-packagekit packagekit system-config-printer gnome-tour
   ];
 
+  # Nerdfonts.
   fonts.packages = with pkgs; [
   (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "FantasqueSansMono" ]; })
 ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+   
+  system.stateVersion = "23.11"; 
 
 }
