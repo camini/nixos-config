d759763a13dacbb2eab43936708afb0b24c6a8a0
Cammi <118620964+camini@users.noreply.github.com>
2024-02-04 01:01:27 +0100

    rewrite

--- AMD-pstate.nix
+++ AMD-pstate.nix
@@ -1,28 +1,7 @@
-{ lib, config, ... }:
-let
-  kver = config.boot.kernelPackages.kernel.version;
-in
-{imports = [ ./. ];
-  boot = lib.mkMerge [
-    (lib.mkIf
-      (
-        (lib.versionAtLeast kver "5.17")
-        && (lib.versionOlder kver "6.1")
-      )
-      {
-        kernelParams = [ "initcall_blacklist=acpi_cpufreq_init" ];
-        kernelModules = [ "amd-pstate" ];
-      })
-    (lib.mkIf
-      (
-        (lib.versionAtLeast kver "6.1")
-        && (lib.versionOlder kver "6.3")
-      )
-      {
-        kernelParams = [ "amd_pstate=passive" ];
-      })
-    (lib.mkIf (lib.versionAtLeast kver "6.3") {
-      kernelParams = [ "amd_pstate=active" ];
-    })
-  ];
+{ config, pkgs, ... }:
+
+{
+  # Activation AMD P-state pour une meilleure gestion energetique du CPU
+   boot.kernelParams = [ "amd_pstate=active" "mitigations=off" ];
 }
+
