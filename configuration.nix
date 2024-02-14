073452e31e3eb1865a3294c49e8539eb7376dc01
Cammi <camini57@gmail.com>
2024-02-14 21:47:28 +0100

    new layout

--- configuration.nix
+++ configuration.nix
@@ -1,83 +1,48 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ...}:
 
 {
-  imports =
-    [ ./hardware-configuration.nix
+  imports = 
+    [
+      ./hardware-configuration.nix
       ./config/VM.nix
       ./config/AMD-pstate.nix
       ./config/Alias.nix      
       ./config/Nix-clean.nix
       ./config/Gnome-clean.nix
-];
+    ];
 
-  # Completion bash.
-  programs.bash.enableCompletion = true;
-   
-  # Boot.
-  boot.plymouth.enable = true;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  #/--------------------\# 
+  #| ° Kernel Version ° |#
+  #\--------------------/#
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  services.fstrim.enable =  true;
 
-  # GPU.
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.opengl.driSupport = true; 
-  hardware.opengl.driSupport32Bit = true; 
-  programs.corectrl.gpuOverclock.enable = true;
-  environment.sessionVariables = {
-  #If your cursor becomes invisible
-  WLR_NO_HARDWARE_CURSORS = "1";
-  #Hint electron apps to use wayland
-  NIXOS_OZONE_WL = "1";
-};
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = false;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+  #/-----------------------------\# 
+  #| ° Boot Loader & Animation ° |#
+  #\-----------------------------/#
+  boot = {
+    loader = {
+     systemd-boot.enable      = true;
+     efi.canTouchEfiVariables = true;
+    };
+    #plymouth = {
+    #  enable = true;
+    #  theme  = "breeze";
+    #};
   };
-   
-  # Nom de la machine.
-  networking.hostName = "nixos"; 
-  
-  # Montage disque Jeux.
-    fileSystems."/home/cammi/Jeux" =
-  { device = "/dev/disk/by-label/Jeux";
-    fsType = "ext4";
-  };
-  
-  # Activation réseau.
-  networking.networkmanager.enable = true;
 
-  # Timezone.
+  #/----------------\# 
+  #| ° Networking ° |#
+  #\----------------/#
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true; 
+  };
+
+  #/----------------------\# 
+  #| ° Time & Languages ° |#
+  #\----------------------/#
   time.timeZone = "Europe/Paris";
-
-  # Locale.
   i18n.defaultLocale = "fr_FR.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "fr_FR.UTF-8";
     LC_IDENTIFICATION = "fr_FR.UTF-8";
@@ -90,51 +55,82 @@
     LC_TIME = "fr_FR.UTF-8";
   };
 
-  # X11.
-  services.xserver.enable = true;
-
-  # Gnome.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Keymap sur X11.
-  services.xserver = {
-    xkb.layout = "fr";
-    xkb.variant = "azerty";
+  #/--------------------------\# 
+  #| ° Keyboard Disposition ° |#
+  #\--------------------------/#
+  services.xserver.xkb = {
+    layout  = "fr";
+    variant = "azerty";
   };
 
-  # Keymap console.
-  console.keyMap = "fr";
-
-  # Pipewire.
+  #/-----------\# 
+  #| ° Sound ° |#
+  #\-----------/#
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+    alsa = {
+      enable       = true;
+      support32Bit = true;
+    };
+    pulse.enable   = true;
+  };
+  
+  #/---------\# 
+  #| ° GPU ° |#
+  #\---------/#
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  }; 
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
-  # Compte utilisateur.
+  #/-----------------------------------\# 
+  #| ° Xserver & Desktop Environment ° |#
+  #\-----------------------------------/#
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable   = true;
+    desktopManager.gnome.enable = true;
+  };
+
+  #/--------------------------\# 
+  #| ° Users Configurations ° |#
+  #\--------------------------/#
+  # ------------ # 
+  #/-----------\#
+  #| ° cammi ° |#
+  #\-----------/#
   users.users.cammi = {
     isNormalUser = true;
     description = "cammi";
-    extraGroups = [ "video" "audio" "usb" "networkmanager" "wheel" ];
+    extraGroups = [ "wheel" "networkmanager" "audio" "video" "usb" ];
   };
 
-  # Paquets non libres.
-  nixpkgs.config.allowUnfree = true;
-  
-  # Mes applications.
+  #/---------------------\# 
+  #| ° System Packages ° |#
+  #\---------------------/#
   environment.systemPackages = with pkgs; [
+   # hardware
+   compsize
+   pciutils
+   usbutils   
    # Editeur/code
    vscode
    bash-completion
    git
    # Theme/visuel
-   plymouth
    adw-gtk3
    qogir-icon-theme
    mangohud
@@ -153,7 +149,6 @@
    obs-studio-plugins.obs-vkcapture
    obs-studio
    vlc
-   corectrl
    # Gnome extensions
    gnome.gnome-tweaks
    gnomeExtensions.blur-my-shell
@@ -161,19 +156,37 @@
    gnomeExtensions.appindicator
    gnomeExtensions.dash-to-dock	
   ];
-  
-  # Installation de Steam.
+    
+  #/-----------------\# 
+  #| ° FileSystems ° |#
+  #|   Mount Options |#
+  #\-----------------/#
+  fileSystems = {
+    "/".options =     [ "subvol=@"     "compress=zstd:3" "noatime" "defaults" "discard"];
+    "/home".options = [ "subvol=@home" "compress=zstd:3" "noatime" "defaults" "discard"];
+  };
+  fileSystems."/home/cammi/Jeux" =
+  { device = "/dev/disk/by-label/Jeux";
+    fsType = "ext4";
+  };
+
+  #/-----------\# 
+  #| ° Steam ° |#
+  #\-----------/#
   programs.steam = {
   enable = true;
   remotePlay.openFirewall = true; 
   dedicatedServer.openFirewall = true; 
-};
- 
-  # Nerdfonts.
-  fonts.packages = with pkgs; [
-  (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "FantasqueSansMono" ]; })
-];
-   
-  system.stateVersion = "23.11"; 
+  };
 
+  #/---------------------\# 
+  #| ° Unfree Software ° |#
+  #\---------------------/#
+  nixpkgs.config.allowUnfree = true;
+
+  #/-------------------------------\# 
+  #| ° Nix Configuration Version ° |#
+  #|   DO NOT MODIFY !!!           |#
+  #\-------------------------------/#
+  system.stateVersion = "23.11";
 }
