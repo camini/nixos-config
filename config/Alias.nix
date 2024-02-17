e1edb87271ad04cf3e0eff0511fa42c76a709e99
Cammi <camini57@gmail.com>
2024-02-17 11:25:52 +0100

    add some aliases

--- config/Alias.nix
+++ config/Alias.nix
@@ -3,6 +3,8 @@
 {
     programs.bash.shellAliases = { 
     nixup = "sudo nixos-rebuild switch --upgrade";
+    cleans = "sudo nix-store --gc";
+    cleang = "sudo nix-collect-garbage -d";
     };
 
 }
