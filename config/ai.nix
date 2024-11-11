4d8af8ee91b624924f7730c3365c9fdf1bb38e71
Cammi <118620964+camini@users.noreply.github.com>
2024-11-11 10:06:40 +0100

    Add files via upload

---
+++ config/ai.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+services.ollama = {
+  enable = true;
+  acceleration = "rocm";
+  environmentVariables = {
+    HCC_AMDGPU_TARGET = "gfx1101"; # used to be necessary, but doesn't seem to anymore
+  };
+  rocmOverrideGfx = "11.0.1";
+};
+}
--- config/gnome.nix
+++ config/gnome.nix
@@ -8,8 +8,8 @@ services.xserver = {
   };
 
 environment.systemPackages = with pkgs; [  
-  gnome.gnome-tweaks
-  gnome.eog
+  gnome-tweaks
+  eog
   gnomeExtensions.gsconnect
   gnomeExtensions.caffeine
   gnomeExtensions.dash-to-dock
@@ -21,12 +21,44 @@ environment.systemPackages = with pkgs; [
 
 services.xserver.excludePackages = [ pkgs.xterm ];
   environment.gnome.excludePackages = with pkgs; [
-    gnome.gnome-music gnome-tour gnome.gnome-weather 
-    gnome.totem gnome.tali gnome.iagno gnome.hitori gnome.atomix gnome.simple-scan
-    gnome.yelp gnome.gnome-maps gnome.gnome-clocks 
-    gnome.geary xterm gnome-user-docs epiphany
-    gnome.gnome-packagekit packagekit system-config-printer gnome-tour
+    gnome-music gnome-tour gnome-weather 
+    totem tali iagno hitori atomix simple-scan
+    yelp gnome-maps gnome-clocks 
+    geary xterm gnome-user-docs epiphany
+    gnome-packagekit packagekit system-config-printer gnome-tour
   ];  
 
+programs.dconf.profiles.user = {
+        databases = [{
+            settings = {
+                # Gnome
+                "org/gnome/desktop/wm/preferences" = {
+                    button-layout = "appmenu:minimize,maximize,close";
+                };
+                "org/gnome/desktop/interface" = {
+                    enable-hot-corners = true;
+                    gtk-theme = "adw-gtk3";
+                    icon-theme = "Tela-circle";
+                };
+                "org/gnome/mutter" = {
+                    check-alive-timeout = lib.gvariant.mkInt32 30000;
+                    dynamic-workspaces = true;
+                    edge-tiling = true;
+                };
+                "org/gnome/desktop/wm/preferences" = {
+                    theme = "adw-gtk3";
+                };
+
+                # Extensions
+                "org/gnome/shell" = {
+                    enabled-extensions = [
+                        "caffeine@patapon.info"
+                        "appindicatorsupport@rgcjonas.gmail.com"
+                        "dash-to-dock@micxgx.gmail.com"
+                    ];     
+                };
+              };
+           }];
+        };
 }
 
